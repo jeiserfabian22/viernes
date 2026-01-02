32434ea655adf75850351fa6928ff82c9fac8f1d
jeiserfabian22 <jeiserfabian@gmail.com>
2026-01-02 11:23:20 -0500

    Fix STATIC_URL and MEDIA_ROOT paths in settings

--- raiz/settings.py
+++ raiz/settings.py
@@ -131,14 +131,11 @@ USE_TZ = False
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/5.0/howto/static-files/
 
-STATIC_URL = 'static/'
+STATIC_URL = '/static/'
 
 STATIC_ROOT = os.path.join(BASE_DIR, 'static/')
 
-STATICFILES_DIRS = [
-    BASE_DIR / "static",
-    "/var/www/static/",
-]
+
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.0/ref/settings/#default-auto-field
 
@@ -146,7 +143,7 @@ DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
 # Configuración de la ruta media
 MEDIA_URL = '/media/'  # URL que se usará para acceder a los archivos media
-MEDIA_ROOT = os.path.join(BASE_DIR, 'media')  # Ruta en el sistema de archivos
+MEDIA_ROOT = os.path.join(BASE_DIR, 'media/')  # Ruta en el sistema de archivos
 
 # Configuración de correo Gmail
 EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
